8e80070a066b2b1f8e93cdeff0926c4edfa02f90
Saltator <saltatorv@gmail.com>
2026-01-27 00:21:10 +0100

    [2]:feat - initialize domain module

---
+++ polaris-flash-cards-domain/src/main/java/com/saltatorv/polaris/flash/cards/domain/PolarisFlashCardsDomainApplication.java
@@ -0,0 +1,13 @@
+package com.saltatorv.polaris.flash.cards.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolarisFlashCardsDomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolarisFlashCardsDomainApplication.class, args);
+    }
+
+}
