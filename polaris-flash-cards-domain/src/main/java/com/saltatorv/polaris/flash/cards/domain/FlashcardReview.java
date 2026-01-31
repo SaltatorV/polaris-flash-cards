147d9a566c82ec1343c79c9c971659612608de0f
Saltator <saltatorv@gmail.com>
2026-01-31 21:06:10 +0100

    [2][domain]:feat - add FlashcardReview::begin to record review start time

--- polaris-flash-cards-domain/src/main/java/com/saltatorv/polaris/flash/cards/domain/FlashcardReview.java
+++ polaris-flash-cards-domain/src/main/java/com/saltatorv/polaris/flash/cards/domain/FlashcardReview.java
@@ -1,9 +1,12 @@
 package com.saltatorv.polaris.flash.cards.domain;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 
 class FlashcardReview {
     private HashMap<String, String> flashcards;
+    private long startTime;
 
     public FlashcardReview(HashMap<String, String> flashcards) {
         this.flashcards = flashcards;
@@ -11,5 +14,15 @@ class FlashcardReview {
 
     public int flashcardCount() {
         return flashcards.size();
+    }
+
+    public void begin() {
+        startTime = System.currentTimeMillis();
+    }
+
+    public LocalDateTime getStartDate() {
+        return LocalDateTime.ofInstant(
+                Instant.ofEpochMilli(startTime),
+                java.time.ZoneId.systemDefault());
     }
 }
--- polaris-flash-cards-domain/src/test/java/com/saltatorv/polaris/flash/cards/domain/FlashcardReviewTest.java
+++ polaris-flash-cards-domain/src/test/java/com/saltatorv/polaris/flash/cards/domain/FlashcardReviewTest.java
@@ -2,8 +2,11 @@ package com.saltatorv.polaris.flash.cards.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
 import static com.saltatorv.polaris.flash.cards.domain.FlashcardReviewBuilder.buildFlashcardReview;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class FlashcardReviewTest {
 
@@ -21,4 +24,24 @@ class FlashcardReviewTest {
         //then
         assertEquals(2, review.flashcardCount());
     }
+
+    @Test
+    public void testShouldGenerateStartDateWhenReviewBegin() {
+        //given
+        FlashcardReview review = buildFlashcardReview()
+                .addFlashcard("Question-1", "Answer-1")
+                .addFlashcard("Question-2", "Answer-2")
+                .create();
+
+        //when
+        review.begin();
+
+        //then
+        assertTrue(review.getStartDate().isBefore(getCurrentDate()));
+    }
+
+    private LocalDateTime getCurrentDate() {
+        return LocalDateTime.now();
+    }
+
 }
