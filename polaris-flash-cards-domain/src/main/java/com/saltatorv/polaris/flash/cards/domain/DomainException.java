78fe64441d26103c8d6ecd7040dd336ed7701a86
Saltator <saltatorv@gmail.com>
2026-02-02 21:16:43 +0100

    [2][domain]:feat - add DomainException

---
+++ polaris-flash-cards-domain/src/main/java/com/saltatorv/polaris/flash/cards/domain/DomainException.java
@@ -0,0 +1,8 @@
+package com.saltatorv.polaris.flash.cards.domain;
+
+@Generated
+public class DomainException extends RuntimeException {
+    public DomainException(String message) {
+        super(message);
+    }
+}
