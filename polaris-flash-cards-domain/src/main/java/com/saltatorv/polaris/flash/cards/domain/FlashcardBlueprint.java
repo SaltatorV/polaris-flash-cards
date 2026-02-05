ec5422f22d7e7b8a17f7b2f6b1ac5989b392b0f9
Saltator <saltatorv@gmail.com>
2026-02-05 23:05:18 +0100

    [2][domain]:fix - FlashcardBlueprint contained FlashcardReviewId

--- polaris-flash-cards-domain/src/main/java/com/saltatorv/polaris/flash/cards/domain/FlashcardBlueprint.java
+++ polaris-flash-cards-domain/src/main/java/com/saltatorv/polaris/flash/cards/domain/FlashcardBlueprint.java
@@ -1,16 +1,17 @@
 package com.saltatorv.polaris.flash.cards.domain;
 
+import com.saltatorv.polaris.flash.cards.domain.shared.FlashcardBlueprintId;
 import com.saltatorv.polaris.flash.cards.domain.shared.FlashcardReviewId;
 
 @Generated
 public class FlashcardBlueprint {
-    private final FlashcardReviewId reviewId;
+    private final FlashcardBlueprintId reviewId;
     private final String question;
     private final String answer;
     private final FlashcardMetadata metadata;
 
     public FlashcardBlueprint(String question, String answer, FlashcardMetadata metadata) {
-        this.reviewId = FlashcardReviewId.generate();
+        this.reviewId = FlashcardBlueprintId.generate();
         this.question = question;
         this.answer = answer;
         this.metadata = metadata;
@@ -24,7 +25,7 @@ public class FlashcardBlueprint {
         return metadata;
     }
 
-    public FlashcardReviewId getReviewId() {
+    public FlashcardBlueprintId getReviewId() {
         return reviewId;
     }
 }
