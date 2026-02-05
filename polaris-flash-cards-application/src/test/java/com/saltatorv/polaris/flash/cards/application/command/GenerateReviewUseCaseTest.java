dc5c04219b5586e8c1edff408d73ed0d4b690218
Saltator <saltatorv@gmail.com>
2026-02-05 22:10:31 +0100

    [2][application]:test - prepare environment for GenerateReviewUseCaseTest

--- polaris-flash-cards-application/src/test/java/com/saltatorv/polaris/flash/cards/application/command/GenerateReviewUseCaseTest.java
+++ polaris-flash-cards-application/src/test/java/com/saltatorv/polaris/flash/cards/application/command/GenerateReviewUseCaseTest.java
@@ -1,13 +1,18 @@
 package com.saltatorv.polaris.flash.cards.application.command;
 
 import com.saltatorv.polaris.flash.cards.application.FlashcardBlueprintIdCache;
+import com.saltatorv.polaris.flash.cards.domain.FlashcardBlueprint;
 import com.saltatorv.polaris.flash.cards.domain.FlashcardBlueprintRepository;
+import com.saltatorv.polaris.flash.cards.domain.FlashcardMetadata;
 import com.saltatorv.polaris.flash.cards.domain.FlashcardReviewRepository;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
-class GenerateReviewUseCaseTest {
+import java.util.ArrayList;
+import java.util.List;
 
+class GenerateReviewUseCaseTest {
     @Mock
     private FlashcardReviewRepository flashcardReviewRepository;
     @Mock
@@ -18,5 +23,23 @@ class GenerateReviewUseCaseTest {
     @InjectMocks
     private GenerateReviewUseCase generateReviewUseCase;
 
+
+    @Test
+    public void testShouldGenerateReview() {
+
+    }
+
+    private List<FlashcardBlueprint> generateFlashcardBlueprints(int size) {
+        List<FlashcardBlueprint> blueprints = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            blueprints.add(new FlashcardBlueprint("Question-%s".formatted(i),
+                    "Answer-%s".formatted(i),
+                    new FlashcardMetadata("Source-%s".formatted(i),
+                            List.of("Tag-%s.1".formatted(i), "Tag-%s.2".formatted(i)),
+                            java.util.Locale.ENGLISH)));
+        }
+
+        return blueprints;
+    }
 
 }
