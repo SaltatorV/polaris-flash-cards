c05ac7ce42bf2ec5ce79630f1efba0f29a50a36d
Saltator <saltatorv@gmail.com>
2026-02-06 23:12:21 +0100

    [2][application]:feat -> use FlashcardReviewDataSnapshot in GetReviewDataUseCase

--- polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/query/GetReviewDataUseCase.java
+++ polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/query/GetReviewDataUseCase.java
@@ -4,49 +4,26 @@ import com.saltatorv.polaris.flash.cards.domain.FlashcardReview;
 import com.saltatorv.polaris.flash.cards.domain.FlashcardReviewRepository;
 import com.saltatorv.polaris.flash.cards.domain.shared.FlashcardReviewId;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
-class GetReviewDataUseCase {
+public class GetReviewDataUseCase {
     private final FlashcardReviewRepository flashcardReviewRepository;
 
     GetReviewDataUseCase(FlashcardReviewRepository flashcardReviewRepository) {
         this.flashcardReviewRepository = flashcardReviewRepository;
     }
 
-    public int flashcardCount(FlashcardReviewId id) {
+    public FlashcardReviewDataSnapshot getReviewData(FlashcardReviewId id) {
         FlashcardReview review = getReview(id);
-
-        return review.flashcardCount();
+        return new FlashcardReviewDataSnapshot(id, review.getCorrectAnswers(),
+                review.getIncorrectAnswers(), review.flashcardCount(),
+                review.getStartDate(), review.getFinishDate());
     }
 
-    public int getCorrectAnswers(FlashcardReviewId id) {
-        FlashcardReview review = getReview(id);
-
-        return review.getCorrectAnswers();
-    }
-
-    public int getIncorrectAnswers(FlashcardReviewId id) {
-        FlashcardReview review = getReview(id);
-
-        return review.getIncorrectAnswers();
-    }
-
-    public LocalDateTime getStartDate(FlashcardReviewId id) {
-        FlashcardReview review = getReview(id);
-
-        return review.getStartDate();
-    }
-
-    public LocalDateTime getFinishDate(FlashcardReviewId id) {
-        FlashcardReview review = getReview(id);
-
-        return review.getFinishDate();
-    }
 
     private FlashcardReview getReview(FlashcardReviewId id) {
         Optional<FlashcardReview> review = flashcardReviewRepository.findById(id);
-        if(review.isEmpty()) {
+        if (review.isEmpty()) {
             throw new IllegalArgumentException("Review not found");
         }
         return review.get();
