f9205e1953b1cc1e5c82f698220ff73bb74b585f
Saltator <saltatorv@gmail.com>
2026-02-06 23:09:34 +0100

    [2][application]:feat -> add FlashcardReviewDataSnapshot

---
+++ polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/query/FlashcardReviewDataSnapshot.java
@@ -0,0 +1,44 @@
+package com.saltatorv.polaris.flash.cards.application.query;
+
+import com.saltatorv.polaris.flash.cards.domain.shared.FlashcardReviewId;
+
+import java.time.LocalDateTime;
+
+public class FlashcardReviewDataSnapshot {
+    private final FlashcardReviewId;
+    private final int correctAnswers;
+    private final int incorrectAnswers;
+    private final int flashcardCount;
+    private final LocalDateTime startDate;
+    private final LocalDateTime finishDate;
+
+    public FlashcardReviewDataSnapshot(int correctAnswers, int incorrectAnswers,
+                                       int flashcardCount, LocalDateTime startDate,
+                                       LocalDateTime finishDate) {
+        this.correctAnswers = correctAnswers;
+        this.incorrectAnswers = incorrectAnswers;
+        this.flashcardCount = flashcardCount;
+        this.startDate = startDate;
+        this.finishDate = finishDate;
+    }
+
+    public int getCorrectAnswers() {
+        return correctAnswers;
+    }
+
+    public int getIncorrectAnswers() {
+        return incorrectAnswers;
+    }
+
+    public int getFlashcardCount() {
+        return flashcardCount;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public LocalDateTime getFinishDate() {
+        return finishDate;
+    }
+}
