26b5850c93150e6c235b514e2c89db5886c5ae45
Saltator <saltatorv@gmail.com>
2026-02-12 23:26:09 +0100

    [2][domain]:feat - update FlashcardSnapshot with question and definition fields to simplify receiving snapshot from db

--- polaris-flash-cards-domain/src/main/java/com/saltatorv/polaris/flash/cards/domain/FlashcardSnapshot.java
+++ polaris-flash-cards-domain/src/main/java/com/saltatorv/polaris/flash/cards/domain/FlashcardSnapshot.java
@@ -2,14 +2,18 @@ package com.saltatorv.polaris.flash.cards.domain;
 
 public class FlashcardSnapshot {
     private final String flashcardBlueprintId;
+    private final String question;
+    private final String definition;
     private final String answer;
 
-    public FlashcardSnapshot(String flashcardBlueprintId, Answer answer) {
-        this(flashcardBlueprintId, answer.name());
+    public FlashcardSnapshot(String flashcardBlueprintId, String question, String definition, Answer answer) {
+        this(flashcardBlueprintId, question, definition, answer.name());
     }
 
-    public FlashcardSnapshot(String flashcardBlueprintId, String answer) {
+    public FlashcardSnapshot(String flashcardBlueprintId, String question, String definition, String answer) {
         this.flashcardBlueprintId = flashcardBlueprintId;
+        this.question = question;
+        this.definition = definition;
         this.answer = answer;
     }
 
@@ -19,5 +23,13 @@ public class FlashcardSnapshot {
 
     public String getAnswer() {
         return answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getDefinition() {
+        return definition;
     }
 }
