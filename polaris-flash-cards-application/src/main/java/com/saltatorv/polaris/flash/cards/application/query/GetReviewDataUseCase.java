f1b9f4d7e669116f417727504fb9a17bf794f210
Saltator <saltatorv@gmail.com>
2026-02-19 23:40:59 +0100

    [2][application]:feat - update GetReviewDataUseCase to use FlashcardReviewUseCaseBase

--- polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/query/GetReviewDataUseCase.java
+++ polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/query/GetReviewDataUseCase.java
@@ -1,16 +1,14 @@
 package com.saltatorv.polaris.flash.cards.application.query;
 
 import com.saltatorv.polaris.flash.cards.application.query.dto.FlashcardReviewDataDto;
+import com.saltatorv.polaris.flash.cards.application.shared.FlashcardReviewUseCaseBase;
 import com.saltatorv.polaris.flash.cards.domain.FlashcardReview;
 import com.saltatorv.polaris.flash.cards.domain.FlashcardReviewRepository;
-import com.saltatorv.polaris.flash.cards.domain.FlashcardReviewSnapshot;
 import com.saltatorv.polaris.flash.cards.domain.shared.FlashcardReviewId;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
-public class GetReviewDataUseCase {
+public class GetReviewDataUseCase extends FlashcardReviewUseCaseBase {
     private final FlashcardReviewRepository flashcardReviewRepository;
 
     GetReviewDataUseCase(FlashcardReviewRepository flashcardReviewRepository) {
@@ -18,18 +16,9 @@ public class GetReviewDataUseCase {
     }
 
     public FlashcardReviewDataDto getReviewData(FlashcardReviewId id) {
-        FlashcardReview review = getReview(id);
+        FlashcardReview review = getReviewFromRepository(id, flashcardReviewRepository);
         return new FlashcardReviewDataDto(id, review.getCorrectAnswers(),
                 review.getIncorrectAnswers(), review.flashcardCount(),
                 review.getStartDate(), review.getFinishDate());
-    }
-
-
-    private FlashcardReview getReview(FlashcardReviewId id) {
-        Optional<FlashcardReviewSnapshot> reviewSnapshot = flashcardReviewRepository.findById(id);
-        if (reviewSnapshot.isEmpty()) {
-            throw new IllegalArgumentException("Review not found");
-        }
-        return FlashcardReview.restore(reviewSnapshot.get());
     }
 }
