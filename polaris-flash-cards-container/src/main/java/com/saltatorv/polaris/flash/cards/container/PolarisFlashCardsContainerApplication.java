f9a7f63d0b387415abb07ee4af2c49db79286682
Saltator <saltatorv@gmail.com>
2026-02-20 23:31:27 +0100

    [2][container]:feat - configure start application

---
+++ polaris-flash-cards-container/src/main/java/com/saltatorv/polaris/flash/cards/container/PolarisFlashCardsContainerApplication.java
@@ -0,0 +1,17 @@
+package com.saltatorv.polaris.flash.cards.container;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EntityScan(basePackages = {"com.saltatorv.polaris.flash.cards"})
+@EnableJpaRepositories(basePackages = {"com.saltatorv.polaris.flash.cards"})
+@SpringBootApplication
+public class PolarisFlashCardsContainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolarisFlashCardsContainerApplication.class, args);
+    }
+
+}
