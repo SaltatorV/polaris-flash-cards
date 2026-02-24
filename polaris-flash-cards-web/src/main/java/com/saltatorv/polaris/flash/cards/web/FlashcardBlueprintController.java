27414b3f14290d1980aebde80fd864eb3f19f8c7
Saltator <saltatorv@gmail.com>
2026-02-24 23:38:16 +0100

    [2][web]:feat - add controller for Flashcard Blueprint

---
+++ polaris-flash-cards-web/src/main/java/com/saltatorv/polaris/flash/cards/web/FlashcardBlueprintController.java
@@ -0,0 +1,25 @@
+package com.saltatorv.polaris.flash.cards.web;
+
+import com.saltatorv.polaris.flash.cards.application.command.blueprint.AddFlashcardBlueprintUseCase;
+import com.saltatorv.polaris.flash.cards.application.command.blueprint.dto.FlashcardBlueprintDataDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/flashcard/blueprints")
+public class FlashcardBlueprintController {
+
+    private final AddFlashcardBlueprintUseCase addFlashcardBlueprintUseCase;
+
+    public FlashcardBlueprintController(AddFlashcardBlueprintUseCase addFlashcardBlueprintUseCase) {
+        this.addFlashcardBlueprintUseCase = addFlashcardBlueprintUseCase;
+    }
+
+    @PostMapping
+    public void createFlashcardBlueprints(List<FlashcardBlueprintDataDto> requests) {
+        addFlashcardBlueprintUseCase.addFlashcardBlueprints(requests);
+    }
+}
