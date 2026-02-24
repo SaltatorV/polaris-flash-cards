6db70591e496c73d3034652cc363656e4d86b34a
Saltator <saltatorv@gmail.com>
2026-02-24 23:11:49 +0100

    [2][core]:feat - update spring from 3.2.2 to 4.0.3

--- polaris-flash-cards-container/src/main/java/com/saltatorv/polaris/flash/cards/container/PolarisFlashCardsContainerApplication.java
+++ polaris-flash-cards-container/src/main/java/com/saltatorv/polaris/flash/cards/container/PolarisFlashCardsContainerApplication.java
@@ -2,7 +2,7 @@ package com.saltatorv.polaris.flash.cards.container;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EntityScan(basePackages = {"com.saltatorv.polaris.flash.cards"})
