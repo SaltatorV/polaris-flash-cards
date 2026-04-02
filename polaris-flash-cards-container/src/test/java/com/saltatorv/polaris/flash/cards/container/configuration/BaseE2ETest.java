8bb77a14cb446620fd12ec7dacf8f5b210e3e8b8
Saltator <saltatorv@gmail.com>
2026-04-03 00:11:35 +0200

    [3][container]:test - use rest assured configuration in base e2e test

--- polaris-flash-cards-container/src/test/java/com/saltatorv/polaris/flash/cards/container/configuration/BaseE2ETest.java
+++ polaris-flash-cards-container/src/test/java/com/saltatorv/polaris/flash/cards/container/configuration/BaseE2ETest.java
@@ -1,28 +1,14 @@
 package com.saltatorv.polaris.flash.cards.container.configuration;
 
-import io.restassured.RestAssured;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static io.restassured.RestAssured.given;
 
-@Testcontainers
 @Import(PostgresConfiguration.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class BaseE2ETest {
-
-    @LocalServerPort
-    private int port;
-
-    @BeforeEach
-    void setup() {
-        RestAssured.baseURI = "http://localhost";
-        RestAssured.port = port;
-    }
+@Testcontainers
+public class BaseE2ETest extends RestAssuredConfiguration {
 
     @Test
     void testShouldAddFlashcardBlueprint() {
