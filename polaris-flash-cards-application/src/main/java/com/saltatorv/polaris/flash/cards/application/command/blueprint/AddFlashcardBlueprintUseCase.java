28be1c104dea0becfe6edcb7120c72d6ccc8664b
Saltator <saltatorv@gmail.com>
2026-04-07 00:37:51 +0200

    [3][application]:feat - invalidate blueprint id cache when add

--- polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/command/blueprint/AddFlashcardBlueprintUseCase.java
+++ polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/command/blueprint/AddFlashcardBlueprintUseCase.java
@@ -1,5 +1,6 @@
 package com.saltatorv.polaris.flash.cards.application.command.blueprint;
 
+import com.saltatorv.polaris.flash.cards.application.FlashcardBlueprintIdCache;
 import com.saltatorv.polaris.flash.cards.application.command.blueprint.dto.FlashcardBlueprintDataDto;
 import com.saltatorv.polaris.flash.cards.domain.FlashcardBlueprint;
 import com.saltatorv.polaris.flash.cards.domain.FlashcardBlueprintRepository;
@@ -11,9 +12,11 @@ import java.util.List;
 @Service
 public class AddFlashcardBlueprintUseCase {
     private final FlashcardBlueprintRepository flashcardBlueprintRepository;
+    private final FlashcardBlueprintIdCache flashcardBlueprintIdCache;
 
-    public AddFlashcardBlueprintUseCase(FlashcardBlueprintRepository flashcardBlueprintRepository) {
+    public AddFlashcardBlueprintUseCase(FlashcardBlueprintRepository flashcardBlueprintRepository, FlashcardBlueprintIdCache flashcardBlueprintIdCache) {
         this.flashcardBlueprintRepository = flashcardBlueprintRepository;
+        this.flashcardBlueprintIdCache = flashcardBlueprintIdCache;
     }
 
     public void addFlashcardBlueprints(List<FlashcardBlueprintDataDto> dtos) {
@@ -23,5 +26,7 @@ public class AddFlashcardBlueprintUseCase {
 
             flashcardBlueprintRepository.save(blueprint.generateSnapshot());
         }
+
+        flashcardBlueprintIdCache.invalidate();
     }
 }
