3bcc1f873932e73f01e2a4d3ea05d9868934405a
Saltator <saltatorv@gmail.com>
2026-04-07 00:01:26 +0200

    [3][web]:fix - update FlashcardBlueprintController to use BaseController

--- polaris-flash-cards-web/src/main/java/com/saltatorv/polaris/flash/cards/web/FlashcardBlueprintController.java
+++ polaris-flash-cards-web/src/main/java/com/saltatorv/polaris/flash/cards/web/FlashcardBlueprintController.java
@@ -4,15 +4,13 @@ import com.saltatorv.polaris.flash.cards.application.command.blueprint.AddFlashc
 import com.saltatorv.polaris.flash.cards.application.command.blueprint.dto.FlashcardBlueprintDataDto;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1")
-public class FlashcardBlueprintController {
-    public static final String FLASHCARD_BLUEPRINT_CREATE_ENDPOINT = "flashcard/blueprint/create";
+public class FlashcardBlueprintController extends BaseController {
+    public static final String FLASHCARD_BLUEPRINT_CREATE_ENDPOINT = "/flashcard/blueprint/create";
 
     private final AddFlashcardBlueprintUseCase addFlashcardBlueprintUseCase;
 
