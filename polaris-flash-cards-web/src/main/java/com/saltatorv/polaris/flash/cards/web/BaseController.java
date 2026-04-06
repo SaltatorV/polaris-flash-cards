a44d27468d623457350c4137d8c81f6b6efc523c
Saltator <saltatorv@gmail.com>
2026-04-06 23:43:04 +0200

    [3][web]:feat - add BaseController

---
+++ polaris-flash-cards-web/src/main/java/com/saltatorv/polaris/flash/cards/web/BaseController.java
@@ -0,0 +1,8 @@
+package com.saltatorv.polaris.flash.cards.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/v1")
+class BaseController {
+    public final static String BASE_API_ENDPOINT = "/api/v1";
+}
