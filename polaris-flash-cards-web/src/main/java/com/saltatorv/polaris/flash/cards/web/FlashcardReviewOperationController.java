42c6ca19736dd8366544526a9e0221e49228aed8
Saltator <saltatorv@gmail.com>
2026-04-07 00:02:52 +0200

    [3][web]:feat - update FlashcardReviewOperationController to use BaseController and move string paths to static variables

--- polaris-flash-cards-web/src/main/java/com/saltatorv/polaris/flash/cards/web/FlashcardReviewOperationController.java
+++ polaris-flash-cards-web/src/main/java/com/saltatorv/polaris/flash/cards/web/FlashcardReviewOperationController.java
@@ -1,40 +1,48 @@
 package com.saltatorv.polaris.flash.cards.web;
 
 import com.saltatorv.polaris.flash.cards.application.command.review.dto.FlashcardDto;
-import com.saltatorv.polaris.flash.cards.application.command.review.operation.*;
+import com.saltatorv.polaris.flash.cards.application.command.review.operation.FlashcardReviewOperationFacade;
 import com.saltatorv.polaris.flash.cards.domain.shared.FlashcardReviewId;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/flashcard/review")
-class FlashcardReviewOperationController {
-   private final FlashcardReviewOperationFacade flashcardReviewOperationFacade;
+class FlashcardReviewOperationController extends BaseController {
+    private final static String BASE_LIFECYCLE_ENDPOINT = "/flashcard/review";
+    private final static String FLASHCARD_REVIEW_BEGIN_ENDPOINT = BASE_LIFECYCLE_ENDPOINT + "/{reviewId}/begin";
+    private final static String FLASHCARD_REVIEW_FINISH_ENDPOINT = BASE_LIFECYCLE_ENDPOINT + "/{reviewId}/finish";
+    private final static String FLASHCARD_REVIEW_DRAW_NEXT_ENDPOINT = BASE_LIFECYCLE_ENDPOINT + "/{reviewId}/flashcard/drawNext";
+    private final static String FLASHCARD_REVIEW_MARK_CORRECT_ENDPOINT = BASE_LIFECYCLE_ENDPOINT + "/{reviewId}/flashcard/markAsCorrect";
+    private final static String FLASHCARD_REVIEW_MARK_INCORRECT_ENDPOINT = BASE_LIFECYCLE_ENDPOINT + "/{reviewId}/flashcard/markAsIncorrect";
+
+    private final FlashcardReviewOperationFacade flashcardReviewOperationFacade;
 
     public FlashcardReviewOperationController(FlashcardReviewOperationFacade flashcardReviewOperationFacade) {
         this.flashcardReviewOperationFacade = flashcardReviewOperationFacade;
     }
 
-    @PostMapping("/{reviewId}/begin")
+    @PostMapping(FLASHCARD_REVIEW_BEGIN_ENDPOINT)
     public void beginReview(@PathVariable("reviewId") String reviewId) {
         flashcardReviewOperationFacade.beginReview(new FlashcardReviewId(reviewId));
     }
 
-    @PostMapping("/{reviewId}/finish")
+    @PostMapping(FLASHCARD_REVIEW_FINISH_ENDPOINT)
     public void finishReview(@PathVariable("reviewId") String reviewId) {
         flashcardReviewOperationFacade.finishReview(new FlashcardReviewId(reviewId));
     }
 
-    @PostMapping("/{reviewId}/flashcard/drawNext")
+    @PostMapping(FLASHCARD_REVIEW_DRAW_NEXT_ENDPOINT)
     public FlashcardDto drawNextFlashcard(@PathVariable("reviewId") String reviewId) {
         return flashcardReviewOperationFacade.nextFlashcard(new FlashcardReviewId(reviewId));
     }
 
-    @PostMapping("/{reviewId}/flashcard/markAsCorrect")
+    @PostMapping(FLASHCARD_REVIEW_MARK_CORRECT_ENDPOINT)
     public void markFlashcardAsCorrect(@PathVariable("reviewId") String reviewId) {
         flashcardReviewOperationFacade.markFlashcardAsCorrect(new FlashcardReviewId(reviewId));
     }
 
-    @PostMapping("/{reviewId}/flashcard/markAsIncorrect")
+    @PostMapping(FLASHCARD_REVIEW_MARK_INCORRECT_ENDPOINT)
     public void markFlashcardAsIncorrect(@PathVariable("reviewId") String reviewId) {
         flashcardReviewOperationFacade.markFlashcardAsIncorrect(new FlashcardReviewId(reviewId));
     }
