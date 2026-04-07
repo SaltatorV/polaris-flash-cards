0714447e59836a8b780ee05f4679591a35c15647
Saltator <saltatorv@gmail.com>
2026-04-07 21:48:04 +0200

    [3][container]:test - clean up BaseE2ETest

--- polaris-flash-cards-container/src/test/java/com/saltatorv/polaris/flash/cards/container/configuration/BaseE2ETest.java
+++ polaris-flash-cards-container/src/test/java/com/saltatorv/polaris/flash/cards/container/configuration/BaseE2ETest.java
@@ -1,31 +1,10 @@
 package com.saltatorv.polaris.flash.cards.container.configuration;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import static io.restassured.RestAssured.given;
-
-@Import(PostgresConfiguration.class)
 @Testcontainers
+@Import(PostgresConfiguration.class)
 public class BaseE2ETest extends RestAssuredConfiguration {
 
-    @Test
-    void testShouldAddFlashcardBlueprint() {
-        //given
-        given()
-                .contentType("application/json")
-                .body("""
-                        [{
-                        "source": "TEST_SOURCE",
-                        "tags": ["Java"],
-                        "language": "EN",
-                        "question": "TEST_QUESTION",
-                        "definition": "TEST_DEFINITION"
-                        }]""")
-                .when()
-                .post("/api/v1/flashcard/blueprints")
-                .then()
-                .statusCode(200);
-    }
 }
