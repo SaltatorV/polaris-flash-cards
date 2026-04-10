8103bb2e97ec9b207cddf1aa30e8afd257d9fb54
Saltator <saltatorv@gmail.com>
2026-04-11 00:59:07 +0200

    [3][container]:test - clean db after each test

--- polaris-flash-cards-container/src/test/java/com/saltatorv/polaris/flash/cards/container/configuration/BaseE2ETest.java
+++ polaris-flash-cards-container/src/test/java/com/saltatorv/polaris/flash/cards/container/configuration/BaseE2ETest.java
@@ -1,10 +1,22 @@
 package com.saltatorv.polaris.flash.cards.container.configuration;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 @Import(PostgresConfiguration.class)
 public class BaseE2ETest extends RestAssuredConfiguration {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
+    @BeforeEach
+    void cleanDatabase() {
+        jdbcTemplate.execute("DO $$ DECLARE r RECORD; BEGIN " +
+                "FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP " +
+                "EXECUTE 'TRUNCATE TABLE ' || quote_ident(r.tablename) || ' RESTART IDENTITY CASCADE'; " +
+                "END LOOP; END $$;");
+    }
 }
