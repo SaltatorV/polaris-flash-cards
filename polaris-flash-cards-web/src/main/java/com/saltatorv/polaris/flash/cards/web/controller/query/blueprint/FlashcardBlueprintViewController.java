5aa6102d6efb0cafb5184ac2f1bd8a7dd07f2f91
Saltator <saltatorv@gmail.com>
2026-04-12 23:18:34 +0200

    [3][web]:feat - add flashcard blueprint and review view controllers

---
+++ polaris-flash-cards-web/src/main/java/com/saltatorv/polaris/flash/cards/web/controller/query/blueprint/FlashcardBlueprintViewController.java
@@ -0,0 +1,9 @@
+package com.saltatorv.polaris.flash.cards.web.controller.query.blueprint;
+
+import com.saltatorv.polaris.flash.cards.web.BaseController;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FlashcardBlueprintViewController extends BaseController {
+
+}
---
+++ polaris-flash-cards-web/src/main/java/com/saltatorv/polaris/flash/cards/web/controller/query/review/FlashcardReviewViewController.java
@@ -0,0 +1,28 @@
+package com.saltatorv.polaris.flash.cards.web.controller.query.review;
+
+import com.saltatorv.polaris.flash.cards.application.query.review.dto.FlashcardReviewDataDto;
+import com.saltatorv.polaris.flash.cards.application.query.review.view.FlashcardReviewViewFacade;
+import com.saltatorv.polaris.flash.cards.domain.shared.FlashcardReviewId;
+import com.saltatorv.polaris.flash.cards.web.BaseController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FlashcardReviewViewController extends BaseController {
+    public final static String BASE_LIFECYCLE_ENDPOINT = "/flashcard/review";
+    public final static String FLASHCARD_REVIEW_GET_ENDPOINT = BASE_LIFECYCLE_ENDPOINT + "/{reviewId}";
+
+    private final FlashcardReviewViewFacade flashcardReviewViewFacade;
+
+    public FlashcardReviewViewController(FlashcardReviewViewFacade flashcardReviewViewFacade) {
+        this.flashcardReviewViewFacade = flashcardReviewViewFacade;
+    }
+
+    @GetMapping(FLASHCARD_REVIEW_GET_ENDPOINT)
+    public FlashcardReviewDataDto getFlashcardReview(@PathVariable String reviewId) {
+        return flashcardReviewViewFacade.getReviewData(new FlashcardReviewId(reviewId));
+    }
+
+
+}
