e3ddf9d4c7934fcf9eac873aef84c984e2bf19b7
Saltator <saltatorv@gmail.com>
2026-05-27 23:47:20 +0200

    [6][domain]:feat - implement UUIDv7 for ID base class

--- polaris-flash-cards-domain/src/main/java/com/saltatorv/polaris/flash/cards/domain/shared/Id.java
+++ polaris-flash-cards-domain/src/main/java/com/saltatorv/polaris/flash/cards/domain/shared/Id.java
@@ -2,12 +2,26 @@ package com.saltatorv.polaris.flash.cards.domain.shared;
 
 import com.saltatorv.polaris.flash.cards.domain.Generated;
 
-@Generated
-class Id<T> {
-    private final T id;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
-    Id(T id) {
-        this.id = id;
+@Generated
+class Id {
+    private final UUID id;
+
+    Id() {
+        this.id = generateUUIDv7();
+    }
+
+    public Id(String id) {
+        this.id = UUID.fromString(id);
+    }
+
+    private UUID generateUUIDv7() {
+        long timestamp = System.currentTimeMillis();
+        long mostSigBits = (timestamp << 16) | 0x7000;
+        long leastSigBits = ThreadLocalRandom.current().nextLong();
+        return new UUID(mostSigBits, leastSigBits);
     }
 
     public String getId() {
