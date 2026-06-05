53e4d8e21cc6bcbfaae34c191e76d617f093a3d3
Saltator <saltatorv@gmail.com>
2026-06-05 23:08:39 +0200

    [8][data-access]:feat - add Category entity

---
+++ polaris-flash-cards-data-access/src/main/java/com/saltatorv/polaris/flash/cards/data/access/entity/Category.java
@@ -0,0 +1,43 @@
+package com.saltatorv.polaris.flash.cards.data.access.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Category {
+    @Id
+    @Column(length = 36)
+    private String id;
+    @Column(length = 36)
+    private String parent;
+    private int depth;
+    @Column(length = 150)
+    private String categoryName;
+
+    public Category() {
+    }
+
+    public Category(String id, String parent, int depth, String categoryName) {
+        this.id = id;
+        this.parent = parent;
+        this.depth = depth;
+        this.categoryName = categoryName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getParent() {
+        return parent;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+}
