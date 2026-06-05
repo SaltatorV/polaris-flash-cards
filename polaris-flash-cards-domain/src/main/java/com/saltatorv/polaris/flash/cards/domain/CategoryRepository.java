b1b2beaaf0ae3580cf356a9ae23e9775e09d0ca7
Saltator <saltatorv@gmail.com>
2026-06-05 21:37:13 +0200

    [8][domain]:feat - add CategoryRepository interface

---
+++ polaris-flash-cards-domain/src/main/java/com/saltatorv/polaris/flash/cards/domain/CategoryRepository.java
@@ -0,0 +1,17 @@
+package com.saltatorv.polaris.flash.cards.domain;
+
+import com.saltatorv.polaris.flash.cards.domain.shared.CategoryId;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CategoryRepository {
+
+    Optional<Category> findById(CategoryId id);
+
+    Category save(Category category);
+
+    void deleteById(CategoryId id);
+
+    List<Category> findByParentId(CategoryId parentId);
+}
