f5e0f9e3583d528408e318325c235efe74dbe6ee
Saltator <saltatorv@gmail.com>
2026-06-05 21:00:30 +0200

    [8][application]:feat - add query dto for FlashcardLocalization

---
+++ polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/query/blueprint/dto/FlashcardLocalizationQueryDto.java
@@ -0,0 +1,27 @@
+package com.saltatorv.polaris.flash.cards.application.query.blueprint.dto;
+
+import java.util.Locale;
+
+class FlashcardLocalizationQueryDto {
+    private final Locale locale;
+    private final String question;
+    private final String answer;
+
+    public FlashcardLocalizationQueryDto(Locale locale, String question, String answer) {
+        this.locale = locale;
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+}
