215c2687c9349959cea490c25e81f08fd0eb2976
Saltator <saltatorv@gmail.com>
2026-06-07 14:45:26 +0200

    [8][domain]:feat - update Category with generateSnapshot and restore

--- polaris-flash-cards-domain/src/main/java/com/saltatorv/polaris/flash/cards/domain/Category.java
+++ polaris-flash-cards-domain/src/main/java/com/saltatorv/polaris/flash/cards/domain/Category.java
@@ -2,6 +2,7 @@ package com.saltatorv.polaris.flash.cards.domain;
 
 import com.saltatorv.polaris.flash.cards.domain.exception.category.CategoryMaxDepthReachedDomainException;
 import com.saltatorv.polaris.flash.cards.domain.shared.CategoryId;
+import com.saltatorv.polaris.flash.cards.domain.snapshot.CategorySnapshot;
 
 public class Category {
     private static final int MAX_AVAILABLE_DEPTH = 4;
@@ -23,6 +24,24 @@ public class Category {
         this.depth = depth;
         this.categoryName = categoryName;
         this.parent = parent;
+    }
+
+    private Category(CategoryId categoryId, String categoryName, int depth, CategoryId parentId) {
+        this.id = categoryId;
+        this.parent = parentId;
+        this.categoryName = categoryName;
+        this.depth = depth;
+    }
+
+    public static Category restore(CategorySnapshot snapshot) {
+        return new Category(new CategoryId(snapshot.getId()),
+                snapshot.getCategoryName(),
+                snapshot.getDepth(),
+                new CategoryId(snapshot.getParentID()));
+    }
+
+    public CategorySnapshot generateSnapshot() {
+        return new CategorySnapshot(id.getId(), depth, categoryName, parent.getId());
     }
 
     public Category createChild(String categoryName) {
