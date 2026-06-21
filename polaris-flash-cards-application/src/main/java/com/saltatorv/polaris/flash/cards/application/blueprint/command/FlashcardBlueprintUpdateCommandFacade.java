acf0b935ff99483d9da0433c6513a7f4d5bd77a2
Saltator <saltatorv@gmail.com>
2026-06-21 23:12:39 +0200

    [9][application]:feat - use blueprint update use cases in blueprint facade

--- polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/blueprint/command/FlashcardBlueprintUpdateCommandFacade.java
+++ polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/blueprint/command/FlashcardBlueprintUpdateCommandFacade.java
@@ -6,11 +6,22 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class FlashcardBlueprintUpdateCommandFacade {
+
+    private final UpdateBlueprintMetadataUseCase updateBlueprintMetadataUseCase;
+    private final UpdateBlueprintLocalizationUseCase updateBlueprintLocalizationUseCase;
+
+    public FlashcardBlueprintUpdateCommandFacade(UpdateBlueprintMetadataUseCase updateBlueprintMetadataUseCase, UpdateBlueprintLocalizationUseCase updateBlueprintLocalizationUseCase) {
+        this.updateBlueprintMetadataUseCase = updateBlueprintMetadataUseCase;
+        this.updateBlueprintLocalizationUseCase = updateBlueprintLocalizationUseCase;
+    }
+
     public void updateBlueprintMetadata(String blueprintId,
                                         FlashcardBlueprintMetadataUpdateDto dto) {
+        updateBlueprintMetadataUseCase.updateBlueprintMetadata(blueprintId, dto);
     }
 
     public void updateLocalization(String blueprintId, String locale,
                                    FlashcardLocalizationUpdateDto dto) {
+        updateBlueprintLocalizationUseCase.updateLocalization(blueprintId, locale, dto);
     }
 }
