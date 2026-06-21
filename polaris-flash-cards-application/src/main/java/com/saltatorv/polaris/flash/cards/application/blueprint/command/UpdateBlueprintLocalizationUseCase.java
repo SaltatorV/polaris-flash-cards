844ef29fccb9bcb4af4f44ed11871173508507df
Saltator <saltatorv@gmail.com>
2026-06-21 23:12:21 +0200

    [9][application]:feat - add use cases for update blueprint

---
+++ polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/blueprint/command/UpdateBlueprintLocalizationUseCase.java
@@ -0,0 +1,47 @@
+package com.saltatorv.polaris.flash.cards.application.blueprint.command;
+
+import com.saltatorv.polaris.flash.cards.application.blueprint.command.dto.FlashcardLocalizationUpdateDto;
+import com.saltatorv.polaris.flash.cards.domain.FlashcardBlueprint;
+import com.saltatorv.polaris.flash.cards.domain.FlashcardBlueprintRepository;
+import com.saltatorv.polaris.flash.cards.domain.FlashcardContent;
+import com.saltatorv.polaris.flash.cards.domain.FlashcardLocalization;
+import com.saltatorv.polaris.flash.cards.domain.exception.blueprint.FlashcardBlueprintWithoutLocalizationDomainException;
+import com.saltatorv.polaris.flash.cards.domain.shared.FlashcardBlueprintId;
+import com.saltatorv.polaris.flash.cards.domain.snapshot.FlashcardBlueprintSnapshot;
+import org.springframework.stereotype.Service;
+
+import java.util.Locale;
+import java.util.Optional;
+
+@Service
+class UpdateBlueprintLocalizationUseCase {
+
+    private final FlashcardBlueprintRepository flashcardBlueprintRepository;
+
+    UpdateBlueprintLocalizationUseCase(FlashcardBlueprintRepository flashcardBlueprintRepository) {
+        this.flashcardBlueprintRepository = flashcardBlueprintRepository;
+    }
+
+    void updateLocalization(String blueprintId, String locale,
+                            FlashcardLocalizationUpdateDto dto) {
+        Optional<FlashcardBlueprintSnapshot> optional = flashcardBlueprintRepository.findById(new FlashcardBlueprintId(blueprintId));
+
+        if (optional.isEmpty()) {
+            throw new IllegalArgumentException("Flashcard blueprint with id " + blueprintId + " does not exist.");
+        }
+
+        FlashcardBlueprint blueprint = FlashcardBlueprint.restore(optional.get());
+
+        try {
+            blueprint.removeLocalization(locale);
+        }
+        catch (FlashcardBlueprintWithoutLocalizationDomainException ex) {
+            // swallow because in next operation it will be added
+        }
+
+        blueprint.addNewLocalization(new FlashcardLocalization(Locale.of(locale),
+                new FlashcardContent(dto.getQuestion(), dto.getAnswer())));
+
+        flashcardBlueprintRepository.save(blueprint.generateSnapshot());
+    }
+}
---
+++ polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/blueprint/command/UpdateBlueprintMetadataUseCase.java
@@ -0,0 +1,35 @@
+package com.saltatorv.polaris.flash.cards.application.blueprint.command;
+
+import com.saltatorv.polaris.flash.cards.application.blueprint.command.dto.FlashcardBlueprintMetadataUpdateDto;
+import com.saltatorv.polaris.flash.cards.domain.FlashcardBlueprint;
+import com.saltatorv.polaris.flash.cards.domain.FlashcardBlueprintRepository;
+import com.saltatorv.polaris.flash.cards.domain.shared.FlashcardBlueprintId;
+import com.saltatorv.polaris.flash.cards.domain.snapshot.FlashcardBlueprintSnapshot;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+class UpdateBlueprintMetadataUseCase {
+
+    private final FlashcardBlueprintRepository flashcardBlueprintRepository;
+
+    UpdateBlueprintMetadataUseCase(FlashcardBlueprintRepository flashcardBlueprintRepository) {
+        this.flashcardBlueprintRepository = flashcardBlueprintRepository;
+    }
+
+
+    void updateBlueprintMetadata(String blueprintId, FlashcardBlueprintMetadataUpdateDto dto) {
+        Optional<FlashcardBlueprintSnapshot> optional = flashcardBlueprintRepository.findById(new FlashcardBlueprintId(blueprintId));
+
+        if (optional.isEmpty()) {
+            throw new IllegalArgumentException("Flashcard blueprint with id " + blueprintId + " does not exist.");
+        }
+
+        FlashcardBlueprint blueprint = FlashcardBlueprint.restore(optional.get());
+
+        blueprint.updateMetadata(dto.getSource(), dto.getTags());
+
+        flashcardBlueprintRepository.save(blueprint.generateSnapshot());
+    }
+}
