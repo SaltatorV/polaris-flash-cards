46b5b7911c248c78fa043e8f1bad311ae01be1d3
Saltator <saltatorv@gmail.com>
2026-06-21 22:02:21 +0200

    [9][application]:feat - define blueprint use cases FlashcardBlueprintDeleteCommandFacade

---
+++ polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/blueprint/command/FlashcardBlueprintDeleteCommandFacade.java
@@ -0,0 +1,17 @@
+package com.saltatorv.polaris.flash.cards.application.blueprint.command;
+
+import com.saltatorv.polaris.flash.cards.application.blueprint.command.dto.FlashcardBlueprintLocalizationDeleteDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FlashcardBlueprintDeleteCommandFacade {
+    public void deleteBlueprintById(String blueprintId) {
+
+    }
+
+    public void deleteBlueprintLocalizations(String blueprintId, List<FlashcardBlueprintLocalizationDeleteDto> dtos) {
+
+    }
+}
