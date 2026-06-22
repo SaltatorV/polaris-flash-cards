dc0ee0fef0b00993ec4785a095f89ba8d5298398
Saltator <saltatorv@gmail.com>
2026-06-22 22:34:53 +0200

    [9][web]:feat - implement controller for query operations on blueprint

--- polaris-flash-cards-web/src/main/java/com/saltatorv/polaris/flash/cards/web/controller/query/blueprint/FlashcardBlueprintViewController.java
+++ polaris-flash-cards-web/src/main/java/com/saltatorv/polaris/flash/cards/web/controller/query/blueprint/FlashcardBlueprintViewController.java
@@ -1,9 +1,37 @@
 package com.saltatorv.polaris.flash.cards.web.controller.query.blueprint;
 
+import com.saltatorv.polaris.flash.cards.application.blueprint.query.FlashcardBlueprintQueryFacade;
+import com.saltatorv.polaris.flash.cards.application.blueprint.query.dto.FlashcardBlueprintQueryDto;
+import com.saltatorv.polaris.flash.cards.application.blueprint.query.dto.FlashcardBlueprintSummaryQueryDto;
 import com.saltatorv.polaris.flash.cards.web.BaseController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class FlashcardBlueprintViewController extends BaseController {
+    private final static String BASE_LIFECYCLE_ENDPOINT = "/flashcard/blueprint";
 
+    public final static String FLASHCARD_BLUEPRINT_GET_ENDPOINT = BASE_LIFECYCLE_ENDPOINT + "/{id}";
+    public final static String FLASHCARD_BLUEPRINT_GET_BY_CATEGORY_ENDPOINT = BASE_LIFECYCLE_ENDPOINT + "/category/{categoryId}";
+
+    private final FlashcardBlueprintQueryFacade flashcardBlueprintQueryFacade;
+
+    public FlashcardBlueprintViewController(FlashcardBlueprintQueryFacade flashcardBlueprintQueryFacade) {
+        this.flashcardBlueprintQueryFacade = flashcardBlueprintQueryFacade;
+    }
+
+
+    @GetMapping(FLASHCARD_BLUEPRINT_GET_ENDPOINT)
+    public FlashcardBlueprintQueryDto getFlashcardBlueprint(@PathVariable String id) {
+        return flashcardBlueprintQueryFacade.getFlashcardBlueprint(id);
+    }
+
+
+    @GetMapping(FLASHCARD_BLUEPRINT_GET_BY_CATEGORY_ENDPOINT)
+    public List<FlashcardBlueprintSummaryQueryDto> getFlashcardBlueprintByCategory(@PathVariable String categoryId) {
+        return flashcardBlueprintQueryFacade.getFlashcardBlueprintSummariesForCategory(categoryId);
+    }
 }
