27fb86c86c9edd3c4f57e8d9eaf9f3a4e959e545
Saltator <saltatorv@gmail.com>
2026-06-22 22:57:30 +0200

    [9][container]:test - update imports for e2e tests

--- polaris-flash-cards-container/src/test/java/com/saltatorv/polaris/flash/cards/container/caller/flashcard/FlashcardReviewEndpointCallerImplementation.java
+++ polaris-flash-cards-container/src/test/java/com/saltatorv/polaris/flash/cards/container/caller/flashcard/FlashcardReviewEndpointCallerImplementation.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 import static com.saltatorv.polaris.flash.cards.web.BaseController.BASE_API_ENDPOINT;
 import static com.saltatorv.polaris.flash.cards.web.controller.command.review.FlashcardReviewLifecycleController.FLASHCARD_REVIEW_GENERATE_RANDOM_ENDPOINT;
 import static com.saltatorv.polaris.flash.cards.web.controller.command.review.FlashcardReviewOperationController.*;
-import static com.saltatorv.polaris.flash.cards.web.controller.query.review.FlashcardReviewViewController.FLASHCARD_REVIEW_GET_ENDPOINT;
+import static com.saltatorv.polaris.flash.cards.web.controller.query.review.FlashcardReviewQueryController.FLASHCARD_REVIEW_GET_ENDPOINT;
 import static io.restassured.RestAssured.given;
 
 public class FlashcardReviewEndpointCallerImplementation implements FlashcardReviewEndpointCaller,
