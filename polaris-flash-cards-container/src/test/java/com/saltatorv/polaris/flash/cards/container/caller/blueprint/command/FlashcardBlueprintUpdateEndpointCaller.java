cd7a697a2c39b38643e9b5ee5c6c2115db93b7d3
Saltator <saltatorv@gmail.com>
2026-06-27 00:03:49 +0200

    [10][container]:test - implement FlashcardBlueprintUpdateEndpointCaller

---
+++ polaris-flash-cards-container/src/test/java/com/saltatorv/polaris/flash/cards/container/caller/blueprint/command/FlashcardBlueprintUpdateEndpointCaller.java
@@ -0,0 +1,54 @@
+package com.saltatorv.polaris.flash.cards.container.caller.blueprint.command;
+
+import com.saltatorv.polaris.flash.cards.application.blueprint.command.dto.FlashcardBlueprintMetadataUpdateDto;
+import com.saltatorv.polaris.flash.cards.application.blueprint.command.dto.FlashcardLocalizationUpdateDto;
+import org.springframework.http.MediaType;
+
+import static com.saltatorv.polaris.flash.cards.web.controller.command.blueprint.FlashcardBlueprintUpdateController.FLASHCARD_BLUEPRINT_UPDATE_LOCALIZATION_ENDPOINT;
+import static com.saltatorv.polaris.flash.cards.web.controller.command.blueprint.FlashcardBlueprintUpdateController.FLASHCARD_BLUEPRINT_UPDATE_METADATA_ENDPOINT;
+import static io.restassured.RestAssured.given;
+
+public class FlashcardBlueprintUpdateEndpointCaller {
+
+    public static FlashcardBlueprintUpdateEndpointCaller build() {
+        return new FlashcardBlueprintUpdateEndpointCaller();
+    }
+
+
+    public FlashcardBlueprintUpdateEndpointCaller updateFlashcardBlueprintMetadata(String blueprintId,
+                                                                                   FlashcardBlueprintMetadataUpdateDto dto) {
+        given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(dto)
+                .when()
+                .patch(configureUpdateMetadataEndpoint(blueprintId))
+                .then()
+                .statusCode(200);
+
+        return this;
+    }
+
+    public FlashcardBlueprintUpdateEndpointCaller updateFlashcardLocalizations(String blueprintId, String locale,
+                                                                               FlashcardLocalizationUpdateDto dto) {
+        given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(dto)
+                .when()
+                .patch(configureUpdateLocalizationEndpoint(blueprintId, locale))
+                .then()
+                .statusCode(200);
+
+
+        return this;
+    }
+
+    private String configureUpdateMetadataEndpoint(String blueprintId) {
+        return FLASHCARD_BLUEPRINT_UPDATE_METADATA_ENDPOINT.replace("{id}", blueprintId);
+    }
+
+    private String configureUpdateLocalizationEndpoint(String blueprintId, String locale) {
+        return FLASHCARD_BLUEPRINT_UPDATE_LOCALIZATION_ENDPOINT.replace("{id}", blueprintId)
+                .replace("{locale}", locale);
+    }
+
+}
