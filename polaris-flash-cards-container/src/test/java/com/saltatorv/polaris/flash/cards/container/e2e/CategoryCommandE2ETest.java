ef7b7bdfc9b4fe99b2279265df375e7227edf270
Saltator <saltatorv@gmail.com>
2026-06-27 00:42:04 +0200

    [10][container]:test - update CategoryCommandE2ETest to use callers

--- polaris-flash-cards-container/src/test/java/com/saltatorv/polaris/flash/cards/container/e2e/CategoryCommandE2ETest.java
+++ polaris-flash-cards-container/src/test/java/com/saltatorv/polaris/flash/cards/container/e2e/CategoryCommandE2ETest.java
@@ -1,17 +1,25 @@
 package com.saltatorv.polaris.flash.cards.container.e2e;
 
 import com.saltatorv.polaris.flash.cards.application.category.command.dto.CategoryDto;
+import com.saltatorv.polaris.flash.cards.container.caller.category.command.CategoryCreationCommandEndpointCaller;
+import com.saltatorv.polaris.flash.cards.container.caller.category.query.CategoryQueryEndpointCaller;
 import com.saltatorv.polaris.flash.cards.container.configuration.BaseE2ETest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 
-import static com.saltatorv.polaris.flash.cards.web.controller.command.category.CategoryCreationController.CATEGORY_CREATE_ENDPOINT;
-import static com.saltatorv.polaris.flash.cards.web.controller.query.category.CategoryQueryController.CATEGORY_GET_ENDPOINT;
-import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class CategoryCommandE2ETest extends BaseE2ETest {
+    // category caller
+    CategoryQueryEndpointCaller categoryQueryEndpointCaller;
+    CategoryCreationCommandEndpointCaller categoryCreationCommandEndpointCaller;
+
+    @BeforeEach
+    public void setup() {
+        categoryQueryEndpointCaller = CategoryQueryEndpointCaller.build();
+        categoryCreationCommandEndpointCaller = CategoryCreationCommandEndpointCaller.build();
+    }
+
 
     @Test
     void testShouldCreateCategory() {
@@ -19,23 +27,10 @@ class CategoryCommandE2ETest extends BaseE2ETest {
         var category = new CategoryDto("Programming", null);
 
         // when
-        given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when()
-                .body(category)
-                .post(CATEGORY_CREATE_ENDPOINT)
-                .then()
-                .statusCode(HttpStatus.OK.value());
+        categoryCreationCommandEndpointCaller.createCategory(category);
 
         // then
-        var listOfCategories = given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when()
-                .get(CATEGORY_GET_ENDPOINT)
-                .then()
-                .statusCode(HttpStatus.OK.value())
-                .extract().jsonPath()
-                .getList(".", com.saltatorv.polaris.flash.cards.application.category.query.dto.CategoryDto.class);
+        var listOfCategories = categoryQueryEndpointCaller.getCategory(1);
 
         var programmingCategory = listOfCategories.stream().filter(c -> c.getCategoryName().equals("Programming")).findFirst();
         var javaCategory = listOfCategories.stream().filter(c -> c.getCategoryName().equals("Java")).findFirst();
