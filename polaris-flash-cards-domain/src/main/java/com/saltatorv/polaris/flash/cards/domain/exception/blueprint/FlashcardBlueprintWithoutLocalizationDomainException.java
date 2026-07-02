15488bca804a10a179c62ab30ee6a8296d511d4e
Saltator <saltatorv@gmail.com>
2026-07-02 22:27:40 +0200

    [10][domain]:feat - fix typo in a message for FlashcardBlueprintWithoutLocalizationDomainException

--- polaris-flash-cards-domain/src/main/java/com/saltatorv/polaris/flash/cards/domain/exception/blueprint/FlashcardBlueprintWithoutLocalizationDomainException.java
+++ polaris-flash-cards-domain/src/main/java/com/saltatorv/polaris/flash/cards/domain/exception/blueprint/FlashcardBlueprintWithoutLocalizationDomainException.java
@@ -3,7 +3,7 @@ package com.saltatorv.polaris.flash.cards.domain.exception.blueprint;
 import com.saltatorv.polaris.flash.cards.domain.exception.DomainException;
 
 public class FlashcardBlueprintWithoutLocalizationDomainException extends DomainException {
-    private final static String MESSAGE = "Flashcard review blueprint must contain at least one localization";
+    private final static String MESSAGE = "Flashcard blueprint must contain at least one localization";
 
     public FlashcardBlueprintWithoutLocalizationDomainException() {
         super(MESSAGE);
