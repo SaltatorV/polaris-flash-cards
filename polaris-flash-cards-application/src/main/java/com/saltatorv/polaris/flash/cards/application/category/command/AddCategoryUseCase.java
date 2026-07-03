2745b91e3a9ba403663fd7f109c6bc0b3ba14dee
Saltator <saltatorv@gmail.com>
2026-07-03 23:47:48 +0200

    [10][application]:feat - update adding a category use case with throwing application exception

--- polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/category/command/AddCategoryUseCase.java
+++ polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/category/command/AddCategoryUseCase.java
@@ -1,6 +1,7 @@
 package com.saltatorv.polaris.flash.cards.application.category.command;
 
 import com.saltatorv.polaris.flash.cards.application.category.command.dto.CategoryDto;
+import com.saltatorv.polaris.flash.cards.application.shared.exception.ApplicationException;
 import com.saltatorv.polaris.flash.cards.domain.Category;
 import com.saltatorv.polaris.flash.cards.domain.CategoryRepository;
 import com.saltatorv.polaris.flash.cards.domain.shared.CategoryId;
@@ -8,6 +9,9 @@ import com.saltatorv.polaris.flash.cards.domain.snapshot.CategorySnapshot;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+
+import static com.saltatorv.polaris.flash.cards.application.category.exception.CategoryExceptionConfiguration.CATEGORY_DUPLICATED;
+import static com.saltatorv.polaris.flash.cards.application.category.exception.CategoryExceptionConfiguration.CATEGORY_NOT_FOUND;
 
 @Service
 class AddCategoryUseCase {
@@ -24,7 +28,9 @@ class AddCategoryUseCase {
         if (categoryDto.getParentCategoryId() != null) {
             Optional<CategorySnapshot> optionalCategory = categoryRepository.findById(new CategoryId(categoryDto.getParentCategoryId()));
             if (optionalCategory.isEmpty()) {
-                throw new IllegalArgumentException("Parent category with id " + categoryDto.getParentCategoryId() + " does not exist.");
+                throw new ApplicationException(CATEGORY_NOT_FOUND,
+                        "Parent category with id: %s does not exist."
+                                .formatted(categoryDto.getParentCategoryId()));
             }
             parentCategory = Category.restore(optionalCategory.get());
         }
@@ -39,8 +45,9 @@ class AddCategoryUseCase {
         Optional<CategorySnapshot> duplicatedCategory = categoryRepository.findByNameAndDepth(categoryToSave.getCategoryName(), categoryToSave.getDepth());
 
         if (duplicatedCategory.isPresent()) {
-            throw new IllegalArgumentException("Category with name: %s already exists on depth level: %s"
-                    .formatted(categoryToSave.getCategoryName(), categoryToSave.getDepth()));
+            throw new ApplicationException(CATEGORY_DUPLICATED,
+                    "Category with name: %s already exists on depth level: %s"
+                            .formatted(categoryToSave.getCategoryName(), categoryToSave.getDepth()));
         }
 
         categoryRepository.save(categoryToSave.generateSnapshot());
--- polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/category/exception/CategoryExceptionConfiguration.java
+++ polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/category/exception/CategoryExceptionConfiguration.java
@@ -3,7 +3,8 @@ package com.saltatorv.polaris.flash.cards.application.category.exception;
 import com.saltatorv.polaris.flash.cards.application.shared.exception.ExceptionConfiguration;
 
 public enum CategoryExceptionConfiguration implements ExceptionConfiguration {
-    ;
+    CATEGORY_NOT_FOUND(404),
+    CATEGORY_DUPLICATED(409);
 
     private final int statusCode;
 
