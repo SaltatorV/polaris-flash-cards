cc6d998fc4d68ab812e47239cc9ac61d10774e74
Saltator <saltatorv@gmail.com>
2026-07-05 21:30:14 +0200

    [10][application]:feat - throw application exceptions when finish review

--- polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/review/command/operation/FinishReviewUseCase.java
+++ polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/review/command/operation/FinishReviewUseCase.java
@@ -1,10 +1,16 @@
 package com.saltatorv.polaris.flash.cards.application.review.command.operation;
 
 import com.saltatorv.polaris.flash.cards.application.review.shared.FlashcardReviewUseCaseBase;
+import com.saltatorv.polaris.flash.cards.application.shared.exception.ApplicationException;
 import com.saltatorv.polaris.flash.cards.domain.FlashcardReview;
 import com.saltatorv.polaris.flash.cards.domain.FlashcardReviewRepository;
+import com.saltatorv.polaris.flash.cards.domain.exception.review.FlashcardReviewAlreadyFinishedDomainException;
+import com.saltatorv.polaris.flash.cards.domain.exception.review.FlashcardReviewNotStartedDomainException;
 import com.saltatorv.polaris.flash.cards.domain.shared.FlashcardReviewId;
 import org.springframework.stereotype.Service;
+
+import static com.saltatorv.polaris.flash.cards.application.review.exception.FlashcardReviewExceptionConfiguration.REVIEW_ALREADY_FINISHED;
+import static com.saltatorv.polaris.flash.cards.application.review.exception.FlashcardReviewExceptionConfiguration.REVIEW_ALREADY_STARTED;
 
 @Service
 class FinishReviewUseCase extends FlashcardReviewUseCaseBase {
@@ -17,7 +23,13 @@ class FinishReviewUseCase extends FlashcardReviewUseCaseBase {
     void finishReview(FlashcardReviewId id) {
         FlashcardReview review = getReviewFromRepository(id, flashcardReviewRepository);
 
-        review.finish();
+        try {
+            review.finish();
+        } catch (FlashcardReviewNotStartedDomainException ex) {
+            throw new ApplicationException(REVIEW_ALREADY_STARTED, ex.getMessage());
+        } catch (FlashcardReviewAlreadyFinishedDomainException ex) {
+            throw new ApplicationException(REVIEW_ALREADY_FINISHED, ex.getMessage());
+        }
 
         flashcardReviewRepository.save(review.generateSnapshot());
     }
