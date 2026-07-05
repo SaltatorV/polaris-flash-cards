fab7b33d803b4363444eddb3b54cc6277a6b5c98
Saltator <saltatorv@gmail.com>
2026-07-05 23:01:11 +0200

    [10][application]:feat - update FlashcardLocalizationUpdateDto with validation

--- polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/blueprint/command/dto/FlashcardLocalizationUpdateDto.java
+++ polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/blueprint/command/dto/FlashcardLocalizationUpdateDto.java
@@ -1,20 +1,26 @@
 package com.saltatorv.polaris.flash.cards.application.blueprint.command.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 public class FlashcardLocalizationUpdateDto {
-    private String locale;
+    @NotNull(message = "Locale cannot be empty")
+    private Locale locale;
+    @NotEmpty(message = "Question cannot be empty")
     private String question;
+    @NotEmpty(message = "Answer cannot be empty")
     private String answer;
 
     public FlashcardLocalizationUpdateDto() {
     }
 
-    public FlashcardLocalizationUpdateDto(String locale, String question, String answer) {
+    public FlashcardLocalizationUpdateDto(Locale locale, String question, String answer) {
         this.locale = locale;
         this.question = question;
         this.answer = answer;
     }
 
-    public String getLocale() {
+    public Locale getLocale() {
         return locale;
     }
 
