f1999cafe152557585966a929720d7a68583fdc8
Saltator <saltatorv@gmail.com>
2026-07-05 21:36:50 +0200

    [10][application]:feat - throw application exception when getting review from database

--- polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/review/exception/FlashcardReviewExceptionConfiguration.java
+++ polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/review/exception/FlashcardReviewExceptionConfiguration.java
@@ -6,7 +6,8 @@ public enum FlashcardReviewExceptionConfiguration implements ExceptionConfigurat
     NO_FLASHCARD_RECEIVED(409),
     REVIEW_ALREADY_STARTED(409),
     REVIEW_ALREADY_FINISHED(409),
-    NO_MORE_QUESTIONS_IN_REVIEW (409);
+    NO_MORE_QUESTIONS_IN_REVIEW (409),
+    REVIEW_NOT_FOUND(404);
 
     private final int statusCode;
 
--- polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/review/shared/FlashcardReviewUseCaseBase.java
+++ polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/review/shared/FlashcardReviewUseCaseBase.java
@@ -1,11 +1,14 @@
 package com.saltatorv.polaris.flash.cards.application.review.shared;
 
+import com.saltatorv.polaris.flash.cards.application.shared.exception.ApplicationException;
 import com.saltatorv.polaris.flash.cards.domain.FlashcardReview;
 import com.saltatorv.polaris.flash.cards.domain.FlashcardReviewRepository;
-import com.saltatorv.polaris.flash.cards.domain.snapshot.FlashcardReviewSnapshot;
 import com.saltatorv.polaris.flash.cards.domain.shared.FlashcardReviewId;
+import com.saltatorv.polaris.flash.cards.domain.snapshot.FlashcardReviewSnapshot;
 
 import java.util.Optional;
+
+import static com.saltatorv.polaris.flash.cards.application.review.exception.FlashcardReviewExceptionConfiguration.REVIEW_NOT_FOUND;
 
 public class FlashcardReviewUseCaseBase {
 
@@ -13,7 +16,7 @@ public class FlashcardReviewUseCaseBase {
         Optional<FlashcardReviewSnapshot> reviewSnapshot = flashcardReviewRepository.findById(id);
 
         if (reviewSnapshot.isEmpty()) {
-            throw new IllegalArgumentException("Review not found");
+            throw new ApplicationException(REVIEW_NOT_FOUND, "Review with id: %s do not exists.".formatted(id.getId()));
         }
 
         return FlashcardReview.restore(reviewSnapshot.get());
