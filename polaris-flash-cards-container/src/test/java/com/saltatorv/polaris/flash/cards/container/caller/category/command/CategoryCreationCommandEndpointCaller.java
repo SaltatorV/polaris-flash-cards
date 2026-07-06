59a4c4e2c03c0838c2ea156e3b5be89f02b2e92f
Saltator <saltatorv@gmail.com>
2026-07-06 21:22:01 +0200

    [10][container]:test - update CategoryCreationCommandEndpointCaller to use Endpoint Caller base

--- polaris-flash-cards-container/src/test/java/com/saltatorv/polaris/flash/cards/container/caller/category/command/CategoryCreationCommandEndpointCaller.java
+++ polaris-flash-cards-container/src/test/java/com/saltatorv/polaris/flash/cards/container/caller/category/command/CategoryCreationCommandEndpointCaller.java
@@ -1,6 +1,7 @@
 package com.saltatorv.polaris.flash.cards.container.caller.category.command;
 
 import com.saltatorv.polaris.flash.cards.application.category.command.dto.CategoryDto;
+import com.saltatorv.polaris.flash.cards.container.caller.EndpointCaller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -8,20 +9,21 @@ import static com.saltatorv.polaris.flash.cards.web.BaseController.BASE_API_ENDP
 import static com.saltatorv.polaris.flash.cards.web.controller.command.category.CategoryCreationController.CATEGORY_CREATE_ENDPOINT;
 import static io.restassured.RestAssured.given;
 
-public class CategoryCreationCommandEndpointCaller {
+public class CategoryCreationCommandEndpointCaller extends EndpointCaller {
 
     public static CategoryCreationCommandEndpointCaller build() {
         return new CategoryCreationCommandEndpointCaller();
     }
 
     public CategoryCreationCommandEndpointCaller createCategory(CategoryDto dto) {
-        given()
+       response = given()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(dto)
                 .when()
                 .post(BASE_API_ENDPOINT + CATEGORY_CREATE_ENDPOINT)
                 .then()
-                .statusCode(HttpStatus.OK.value());
+                .extract()
+                .response();
 
         return this;
     }
