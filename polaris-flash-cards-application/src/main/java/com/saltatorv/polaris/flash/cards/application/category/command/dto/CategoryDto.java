060394d156624d18c631122f30451d299d477f2c
Saltator <saltatorv@gmail.com>
2026-07-06 20:56:43 +0200

    [10][application]:feat - add validation to command CategoryDto

--- polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/category/command/dto/CategoryDto.java
+++ polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/category/command/dto/CategoryDto.java
@@ -1,7 +1,15 @@
 package com.saltatorv.polaris.flash.cards.application.category.command.dto;
 
+import com.saltatorv.polaris.flash.cards.application.shared.validation.OptionalUUID;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public class CategoryDto {
+
+    @NotEmpty(message = "Category Name cannot be empty")
+    @Size(min = 3, max = 100, message = "Category Name must be between {min} and {max} characters")
     private String categoryName;
+    @OptionalUUID(message = "Parent category ID must be empty or valid UUID")
     private String parentCategoryId;
 
     public CategoryDto(String categoryName, String parentCategoryId) {
