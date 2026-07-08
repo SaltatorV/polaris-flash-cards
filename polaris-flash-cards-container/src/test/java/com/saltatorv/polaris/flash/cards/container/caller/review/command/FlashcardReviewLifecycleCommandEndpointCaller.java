5e4b61b353e9bd7e10bd21ea819ac3ba4ace1e4d
Saltator <saltatorv@gmail.com>
2026-07-08 22:30:32 +0200

    [10][container]:test - update FlashcardReviewLifecycleCommandEndpointCaller to extend EndpointCaller

--- polaris-flash-cards-container/src/test/java/com/saltatorv/polaris/flash/cards/container/caller/review/command/FlashcardReviewLifecycleCommandEndpointCaller.java
+++ polaris-flash-cards-container/src/test/java/com/saltatorv/polaris/flash/cards/container/caller/review/command/FlashcardReviewLifecycleCommandEndpointCaller.java
@@ -1,25 +1,28 @@
 package com.saltatorv.polaris.flash.cards.container.caller.review.command;
 
+import com.saltatorv.polaris.flash.cards.container.caller.EndpointCaller;
 import org.springframework.http.MediaType;
 
 import static com.saltatorv.polaris.flash.cards.web.BaseController.BASE_API_ENDPOINT;
 import static com.saltatorv.polaris.flash.cards.web.controller.command.review.FlashcardReviewLifecycleController.FLASHCARD_REVIEW_GENERATE_RANDOM_ENDPOINT;
 import static io.restassured.RestAssured.given;
 
-public class FlashcardReviewLifecycleCommandEndpointCaller {
+public class FlashcardReviewLifecycleCommandEndpointCaller extends EndpointCaller {
 
     public static FlashcardReviewLifecycleCommandEndpointCaller build() {
         return new FlashcardReviewLifecycleCommandEndpointCaller();
     }
 
     public String generateRandomFlashcardReview(int flashcardCount) {
-        return given()
+        response = given()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when()
                 .body(flashcardCount)
                 .post(BASE_API_ENDPOINT + FLASHCARD_REVIEW_GENERATE_RANDOM_ENDPOINT)
                 .then()
                 .extract()
-                .asString();
+                .response();
+
+        return response.asString();
     }
 }
