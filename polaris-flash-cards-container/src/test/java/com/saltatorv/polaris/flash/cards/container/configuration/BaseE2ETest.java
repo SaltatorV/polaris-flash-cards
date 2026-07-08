aefb18e56f8461bc100b733cf1bec531015c11a4
Saltator <saltatorv@gmail.com>
2026-07-08 23:13:25 +0200

    [10][container]:test - update BaseE2ETest with object mapper and mapping to json method

--- polaris-flash-cards-container/src/test/java/com/saltatorv/polaris/flash/cards/container/configuration/BaseE2ETest.java
+++ polaris-flash-cards-container/src/test/java/com/saltatorv/polaris/flash/cards/container/configuration/BaseE2ETest.java
@@ -5,12 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
 @Testcontainers
 @Import(PostgresConfiguration.class)
 public class BaseE2ETest extends RestAssuredConfiguration {
     @Autowired
     private JdbcTemplate jdbcTemplate;
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @BeforeEach
     void cleanDatabase() {
@@ -20,5 +23,13 @@ public class BaseE2ETest extends RestAssuredConfiguration {
                 "END LOOP; END $$;");
 
         jdbcTemplate.execute("INSERT INTO category_entity (id, category_name, depth) values ('01976e3e-6c52-7000-8c3f-2c4e5d6f7a8b', 'Java', 1)");
+    }
+
+    public String createJsonFrom(Object object) {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Could not create JSON from object", e);
+        }
     }
 }
