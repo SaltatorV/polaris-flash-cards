07a7b7bcbb0de3705e84be0558e8f3190664e1bf
Saltator <saltatorv@gmail.com>
2026-07-15 23:57:12 +0200

    [10][container]:test - create base file for blueprint e2e tests

---
+++ polaris-flash-cards-container/src/test/java/com/saltatorv/polaris/flash/cards/container/e2e/FlashcardBlueprintE2ETest.java
@@ -0,0 +1,6 @@
+package com.saltatorv.polaris.flash.cards.container.e2e;
+
+import com.saltatorv.polaris.flash.cards.container.configuration.BaseE2ETest;
+
+class FlashcardBlueprintE2ETest extends BaseE2ETest {
+}
