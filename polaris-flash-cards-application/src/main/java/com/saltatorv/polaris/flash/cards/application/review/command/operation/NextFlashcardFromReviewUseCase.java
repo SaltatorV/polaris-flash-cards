a5f654971ac09f56dc1f824eff4b09fbe0fa4b77
Saltator <saltatorv@gmail.com>
2026-09-15 22:50:42 +0200

    [11][application]:feat - update NextFlashcardFromReviewUseCase::nextFlashcard to operate on Flashcard Snapshot

--- polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/review/command/operation/NextFlashcardFromReviewUseCase.java
+++ polaris-flash-cards-application/src/main/java/com/saltatorv/polaris/flash/cards/application/review/command/operation/NextFlashcardFromReviewUseCase.java
@@ -3,13 +3,13 @@ package com.saltatorv.polaris.flash.cards.application.review.command.operation;
 import com.saltatorv.polaris.flash.cards.application.review.command.dto.FlashcardDto;
 import com.saltatorv.polaris.flash.cards.application.review.shared.FlashcardReviewUseCaseBase;
 import com.saltatorv.polaris.flash.cards.application.shared.exception.ApplicationException;
-import com.saltatorv.polaris.flash.cards.domain.Flashcard;
 import com.saltatorv.polaris.flash.cards.domain.FlashcardReview;
 import com.saltatorv.polaris.flash.cards.domain.FlashcardReviewRepository;
 import com.saltatorv.polaris.flash.cards.domain.exception.review.FlashcardReviewAlreadyFinishedDomainException;
 import com.saltatorv.polaris.flash.cards.domain.exception.review.FlashcardReviewNotStartedDomainException;
 import com.saltatorv.polaris.flash.cards.domain.exception.review.NoMoreQuestionsInFlashcardReviewDomainException;
 import com.saltatorv.polaris.flash.cards.domain.shared.FlashcardReviewId;
+import com.saltatorv.polaris.flash.cards.domain.snapshot.FlashcardSnapshot;
 import org.springframework.stereotype.Service;
 
 import static com.saltatorv.polaris.flash.cards.application.review.exception.FlashcardReviewExceptionConfiguration.*;
@@ -26,7 +26,7 @@ class NextFlashcardFromReviewUseCase extends FlashcardReviewUseCaseBase {
     FlashcardDto nextFlashcard(FlashcardReviewId id) {
         FlashcardReview review = getReviewFromRepository(id, flashcardReviewRepository);
 
-        Flashcard nextFlashcard;
+        FlashcardSnapshot nextFlashcard;
         try {
             nextFlashcard = review.next();
             FlashcardDto dto = new FlashcardDto(nextFlashcard.getQuestion(),
